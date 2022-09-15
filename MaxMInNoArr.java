fcebdf189f5a9105b75643ccac1c5c989e50d626
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-09-15 23:50:33 +0530

    Add files via upload

---
+++ MaxMInNoArr.java
@@ -0,0 +1,29 @@
+public class MaxMInNoArr {
+    public void max(int arr[]) {
+        int max = arr[0];
+        for (int i = 1; i < 3; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println("MAX num in  array is :" + max);
+    }
+
+       public void min(int arr[]){
+        int min = arr[0] ;
+        for (int i = 1 ; i < 3; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println("MAX num in  array is :" + min);
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {44, 11 , 2};
+        MaxMInNoArr m = new MaxMInNoArr();
+        m.max(arr);
+        m.min(arr);
+    }
+}
